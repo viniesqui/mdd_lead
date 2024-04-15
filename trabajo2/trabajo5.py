2d5261e1209ac5e324d5448a67d71b81b0d8b98a
vinie <v.esquivelestrada@student.utwente.nl>
2024-04-15 10:44:27 -0600

    trabajo 5

---
+++ trabajo2/trabajo5.py
@@ -0,0 +1,58 @@
+from typing import List
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+
+
+class Usuario(BaseModel):
+    id: int
+    username: str
+    email: str
+    password: str
+
+
+class Tarea(BaseModel):
+    id: int
+    title: str
+    description: str
+    status: str
+    user_id: int
+
+
+class AlmacenDatos:
+    def __init__(self):
+        self.usuarios = []
+        self.tareas = []
+
+
+app = FastAPI()
+
+
+def obtener_almacen_datos():
+    return AlmacenDatos()
+
+
+@app.post("/registro")
+def registrar_usuario(usuario: Usuario, almacen_datos: AlmacenDatos = Depends(obtener_almacen_datos)):
+    almacen_datos.usuarios.append(usuario)
+    return {"mensaje": "Usuario registrado exitosamente"}
+
+
+@app.get("/usuario/{id}")
+def obtener_usuario(id: int, almacen_datos: AlmacenDatos = Depends(obtener_almacen_datos)):
+    for usuario in almacen_datos.usuarios:
+        if usuario.id == id:
+            return usuario
+    return {"mensaje": "Usuario no encontrado"}
+
+
+@app.post("/tareas/crear")
+def crear_tarea(tarea: Tarea, almacen_datos: AlmacenDatos = Depends(obtener_almacen_datos)):
+    almacen_datos.tareas.append(tarea)
+    return {"mensaje": "Tarea creada exitosamente"}
+
+
+@app.get("/tareas/{user_id}")
+def obtener_tareas(user_id: int, almacen_datos: AlmacenDatos = Depends(obtener_almacen_datos)):
+    tareas_usuario = [
+        tarea for tarea in almacen_datos.tareas if tarea.user_id == user_id]
+    return tareas_usuario
