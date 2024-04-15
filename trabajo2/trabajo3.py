9a7721cf31a909a306ef52d8dbcf943a3c7b3ac6
vinie <v.esquivelestrada@student.utwente.nl>
2024-04-15 08:07:53 -0600

    Prueba de commit git

---
+++ trabajo2/trabajo3.py
@@ -0,0 +1,8 @@
+#ejemplo para subir algo a git
+
+def sumas(a, b):
+    if isinstance(a and b, int or float):
+        return a + b
+    else: 
+        return("Error: los valores ingresados no son números")
+    
